3587fda37dca6ce643b6d0a661ab26d57802404b
AtharvSharmaAI <b24cm1013@iitj.ac.in>
2025-12-09 01:41:05 +0530

    update in repeat_list_add function to handle datatype error correctly (issue #8996)

--- src/lfortran/parser/semantics.h
+++ src/lfortran/parser/semantics.h
@@ -1873,10 +1873,24 @@ return make_Program_t(al, a_loc,
 #define PLIST_ADD(l, x) l.push_back(p.m_a, *x)
 static inline void repeat_list_add(Vec<ast_t*> &v, Allocator &al,
         ast_t *repeat, ast_t *e) {
-    int64_t n = LCompilers::LFortran::AST::down_cast2<LCompilers::LFortran::AST::Num_t>(repeat)->m_n;
-    for (int64_t i=0; i<n; i++) {
-        v.push_back(al, e);
-    }
+
+        if (LCompilers::LFortran::AST::is_a<LCompilers::LFortran::AST::Num_t>(*repeat)) {
+                auto *num = LCompilers::LFortran::AST::down_cast2<LCompilers::LFortran::AST::Num_t>(repeat);
+
+                int64_t n;
+                n = num->m_n;
+
+                for (int64_t i = 0; i < n; i++) {
+                v.push_back(al, e);
+                }
+        }
+        else{
+               v.push_back(al,e); 
+        }
+
+        return;
+
+
 }
 #define REPEAT_LIST_ADD(l, r, x) repeat_list_add(l, p.m_a, r, x)
 
