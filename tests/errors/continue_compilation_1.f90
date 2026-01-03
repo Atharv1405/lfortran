d45603884fc8b0480ddb6642f04af3699addd290
AtharvSharmaAI <b24cm1013@iitj.ac.in>
2026-01-03 18:59:18 +0530

    updated files 2

--- tests/errors/continue_compilation_1.f90
+++ tests/errors/continue_compilation_1.f90
@@ -175,8 +175,8 @@ program continue_compilation_1
     integer :: fmt_i1, fmt_i2, fmt_i3 ! for issue #8925
     integer, allocatable :: allocate_int = 1
     character(:), allocatable :: allocate_char = "H"
-
-
+    integer :: arrays(3) = [5, 10, 15]
+    integer, parameter :: result = minloc(arrays, 1, [.false., .false., .false.])
 
 
 
